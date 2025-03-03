4942644421f70d5d44c53142d2c47745393ec694
Utkarsh Nageshwar <utkarshnageshwar@gmail.com>
2025-03-03 21:54:39 +0530

    u

--- p.cpp
+++ p.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// int a = 900; //global variable
 
 int main() {
     // int a,b;
@@ -40,5 +41,83 @@ int main() {
     // cin >> num2;
     // (num1 > num2) ? cout << num1 : cout << num2;
 
-    
+    //scope resolution operator
+    //:- it is used to access the global variable
+
+    // {
+    //     int a = 100; //local variable
+    //     cout << a << endl;
+    //     cout << ::a << endl;// scope resolution operator
+    // }
+
+    //referrence variable example
+//     int a = 10;//a is a normal variable
+//     int &b = a;//b is a referrence variable
+//     cout << "a = " <<a << endl; //value of a
+//     cout << "b = " <<b << endl;//value of b
+//     cout<<"address of a = "<<&a<<endl;//address of a
+//     cout<<"address of b = "<<&b<<endl;//address of b
+//     b = 90;
+//     cout << "a = " <<a << endl;
+//     cout << "b = " <<b << endl;
+
+    //pointer variable example
+//   int a = 300;
+//   int b = 900;
+//   int *p;// * is pointer
+//   p=&a;//reference (& is address/reference)
+//   cout<< "address of a = "<<&a<<endl;
+//   cout<<*p;//dereference
+
+   //Q1 write a program to print,subtract,multiply,modulus% & division/ of two numbers;
+//    int a,b;
+//    cout<<"enter 1 no = " <<endl;
+//    cin>>a;
+//     cout<<"enter 2 no = "<<endl;
+//     cin>>b;
+//     cout<<"the ans of a+b is = "<<a+b<<endl;
+//     cout<<"the ans of a-b is = "<<a-b<<endl;
+//     cout<<"the ans of a*b is = "<<a*b<<endl;
+//     cout<<"the ans of a/b is = "<<a/b<<endl;
+//     cout<<"the ans of a%b is = "<<a%b<<endl;
+
+
+   //Q2 write a program to find area of circle;
+    //  int r;
+    //     cout<<"enter the radius of circle = "<<endl;
+    //     cin>>r;
+    //     cout<<"the area of circle is = "<<3.14*r*r<<endl;
+
+
+   //Q3 write a program to find simple interest;
+    // int p,t,r;
+    // cout<<"enter the principle amount = "<<endl;
+    // cin>>p;
+    // cout<<"enter the time = "<<endl;
+    // cin>>t;
+    // cout<<"enter the rate = "<<endl;
+    // cin>>r;
+    // cout<<"the simple interest is = "<<(p*t*r)/100<<endl;
+
+
+   //Q4 write a program to convert celsius to fahrenheit;
+    // int c;
+    // cout<<"enter the celsius = "<<endl;
+    // cin>>c;
+    // cout<<"the fahrenheit is = "<<(c*9/5)+32<<endl;
+
+
+   //Q5 write a program to find square root of a number;
+    // int a;
+    // cout<<"enter the number = "<<endl;
+    // cin>>a;
+    // cout<<"the square root of a number is = "<<a*a<<endl;
+
+
+   //Q6 write a program to find cube of a number;
+    // int a;
+    // cout<<"enter the number = "<<endl;
+    // cin>>a;
+    // cout<<"the cube of a number is = "<<a*a*a<<endl;
+
 }
